786e3d0fcc0b39c1f6a3fcb1fbe41406b99ce01a
sheyguys <sameyu.ping@gmail.com>
2020-01-08 00:58:30 +0700

    Updates

---
+++ precache-manifest.ae6d50bc9a14c435c1f197b5eb0de18f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1730b56c953f2af4e830b4ff55fd16dd",
+    "url": "/BMI-Calculate/index.html"
+  },
+  {
+    "revision": "ae4d70c18b2c8b42c845",
+    "url": "/BMI-Calculate/static/css/main.a147023a.chunk.css"
+  },
+  {
+    "revision": "5ecc5c94981804fd3102",
+    "url": "/BMI-Calculate/static/js/2.a2a4aa8f.chunk.js"
+  },
+  {
+    "revision": "ae4d70c18b2c8b42c845",
+    "url": "/BMI-Calculate/static/js/main.8e682ec0.chunk.js"
+  },
+  {
+    "revision": "23dd0c72e16980ae9f72",
+    "url": "/BMI-Calculate/static/js/runtime-main.a99c9944.js"
+  },
+  {
+    "revision": "4f31faeaf04a52c664e6d98df030538f",
+    "url": "/BMI-Calculate/static/media/0.4f31faea.png"
+  },
+  {
+    "revision": "c94484b7e73d6ca2a0b0d881ab4364e3",
+    "url": "/BMI-Calculate/static/media/1.c94484b7.png"
+  },
+  {
+    "revision": "924f264a06d848beed84e634eff60f6e",
+    "url": "/BMI-Calculate/static/media/2.924f264a.png"
+  },
+  {
+    "revision": "eb0cd4a43b4f9d60c68305817074c9ff",
+    "url": "/BMI-Calculate/static/media/3.eb0cd4a4.png"
+  },
+  {
+    "revision": "774226c3b53c618d24596e0d99c71cfa",
+    "url": "/BMI-Calculate/static/media/4.774226c3.png"
+  },
+  {
+    "revision": "2e9204b100af375dc3d5d6be918845fc",
+    "url": "/BMI-Calculate/static/media/5.2e9204b1.png"
+  },
+  {
+    "revision": "fbd45d65d0850ed19a23825b8190e1a6",
+    "url": "/BMI-Calculate/static/media/Modal.fbd45d65.jpg"
+  },
+  {
+    "revision": "432754a073447124186fcdfc09e90613",
+    "url": "/BMI-Calculate/static/media/asia-map.432754a0.png"
+  },
+  {
+    "revision": "b866622e6f71992678de51320ea16a9c",
+    "url": "/BMI-Calculate/static/media/bg.b866622e.jpg"
+  },
+  {
+    "revision": "013331dafa09313e7da5e65ae6266e1b",
+    "url": "/BMI-Calculate/static/media/europe-map.013331da.png"
+  },
+  {
+    "revision": "e356c45aa6b0b7a77d13afc1c9bf070a",
+    "url": "/BMI-Calculate/static/media/man.e356c45a.png"
+  },
+  {
+    "revision": "b702632c379669a7aff671d001d082a2",
+    "url": "/BMI-Calculate/static/media/women.b702632c.png"
+  },
+  {
+    "revision": "047e0775334f8db597b636eafeadae5c",
+    "url": "/BMI-Calculate/static/media/zero.047e0775.png"
+  }
+]);
